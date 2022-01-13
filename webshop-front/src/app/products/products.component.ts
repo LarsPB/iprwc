708ebbe9c0ac2a175ed59504e3e241807ac4b503
Lars-PC <lars_1994@live.nl>
2022-01-13 15:53:36 +0100

    Fixed some mobile compatibility issues

--- webshop-front/src/app/products/products.component.ts
+++ webshop-front/src/app/products/products.component.ts
@@ -5,7 +5,8 @@ import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html'  
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']  
 })
 export class ProductsComponent implements OnInit, OnDestroy {
   selectedProduct: Product;
