c8189ff4027300c541e19c2f1a00b00b3bf57234
Lars-PC <lars_1994@live.nl>
2022-01-21 12:49:14 +0100

    changed cors

--- webshop-back/app.js
+++ webshop-back/app.js
@@ -21,33 +21,10 @@ mongoose.connect("mongodb+srv://Lars:" + process.env.MONGO_ATLAS_PW + "@cluster0
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    credentials:true
-  })
-);
+app.use(cors());
 
 // To detect and recognise the angular scripts
 app.use("/", express.static(path.join(__dirname,"angular")));
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//     res.setHeader(
-//       'Access-Control-Allow-Methods',
-//       'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-//     );
-//     next();
-// });
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//   next();
-// });
 
 app.use("/api/products", productsRoutes);
 app.use("/api/user", userRoutes);
